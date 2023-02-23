6d77204207d431d641fd38e30e597ea6415df5fb
Reem Gawad <reemabdelg@gmail.com>
2023-02-23 10:25:37 -0500

    fixed turbostream channels

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,5 @@
 class Quote < ApplicationRecord
+  belongs_to :company
   validates :name, presence: true
   scope :ordered, -> { order(id: :desc) }
   # after_create_commit -> { broadcast_prepend_to "quotes", partial: "quotes/quote", locals: { quote: self }, target: "quotes" }
@@ -8,6 +9,5 @@ class Quote < ApplicationRecord
   # after_destroy_commit -> { broadcast_remove_to 'quotes' }
 
   # Those three callbacks ^ are equivalent to the following single line:
-  broadcasts_to ->(quote) { 'quotes' }, inserts_by: :prepend
-  belongs_to :company
+  broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 end
